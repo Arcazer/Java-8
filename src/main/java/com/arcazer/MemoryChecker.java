bc4c80f8eacc8b7d0113e13699451054c758d107
Arcazer <truongkim7@googlemail.com>
2021-04-24 12:23:05 +0200

    refactor string output

--- src/main/java/com/arcazer/MemoryChecker.java
+++ src/main/java/com/arcazer/MemoryChecker.java
@@ -6,6 +6,9 @@ import java.util.ArrayList;
 public class MemoryChecker {
 	private static final long MEBIBYTE = 1024L * 1024L;
 
+	private MemoryChecker() {
+	}
+
 	public static long bytesToMebibytes(long bytes) {
 		return bytes / MEBIBYTE;
 	}
@@ -20,29 +23,29 @@ public class MemoryChecker {
 			list.add(String.valueOf(Math.random()));
 		}
 
-		System.out.printf("String length:%s\n",list.get(0).length());
-		System.out.printf("String size in bytes: %d\n", list.get(0).getBytes("U9TF-8").length);
+		System.out.printf("String length:%s%n", list.get(0).length());
+		System.out.printf("String size in bytes: %d%n", list.get(0).getBytes("UTF-8").length);
 		Runtime runtime = Runtime.getRuntime();
 
-		System.out.printf("Total memory in bytes: %d\n", runtime.totalMemory());
-		System.out.printf("Total memory in mebibytes: %d\n\n", bytesToMebibytes(runtime.totalMemory()));
+		System.out.printf("Total memory in bytes: %d%n", runtime.totalMemory());
+		System.out.printf("Total memory in mebibytes: %d%n%n", bytesToMebibytes(runtime.totalMemory()));
 
 		long memoryBeforeGC = runtime.totalMemory() - runtime.freeMemory();
-		System.out.printf("Used memory in bytes: %s\n", memoryBeforeGC);
-		System.out.printf("Used memory in mebibytes: %d\n\n", bytesToMebibytes(memoryBeforeGC));
+		System.out.printf("Used memory in bytes: %s%n", memoryBeforeGC);
+		System.out.printf("Used memory in mebibytes: %d%n%n", bytesToMebibytes(memoryBeforeGC));
 
 		// garbage collector
 		runtime.gc();
 
 		long memoryAfterGC = runtime.totalMemory() - runtime.freeMemory();
-		System.out.printf("Used memory in bytes: %s\n", memoryAfterGC);
-		System.out.printf("Used memory in mebibytes: %d\n\n ", bytesToMebibytes(memoryAfterGC));
+		System.out.printf("Used memory in bytes: %s%n", memoryAfterGC);
+		System.out.printf("Used memory in mebibytes: %d%n%n ", bytesToMebibytes(memoryAfterGC));
 
 		list = null;
 		runtime.gc();
 
 		long memoryAfterGCAndListClear = runtime.totalMemory() - runtime.freeMemory();
-		System.out.printf("Used memory in bytes: %s\n", memoryAfterGCAndListClear);
-		System.out.printf("Used memory in mebibytes: %d\n\n ", bytesToMebibytes(memoryAfterGCAndListClear));
+		System.out.printf("Used memory in bytes: %s%n", memoryAfterGCAndListClear);
+		System.out.printf("Used memory in mebibytes: %d%n%n ", bytesToMebibytes(memoryAfterGCAndListClear));
 	}
 }
